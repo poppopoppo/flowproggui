19ddba93e5eab7f9d874601dd7125f647ce8416a
poppopoppo <g6.8133251p2@gmail.com>
2019-09-18 16:11:06 +0900

    slv

--- vm/ir0/F0.s
+++ vm/ir0/F0.s
@@ -4,7 +4,7 @@
 .global main
 fact:
 # 	|» 0'
-# 	$ 0' ⊢ ,1',2'
+# 	$ 0' ⊢ ,1',2' : ,@[0].(ℤn)◂(ℤ_u) ≃ ∐[{ } { } ],@[0].(ℤn)◂(ℤ_u) ≃ ∐[{ } { } ]
 	mov r8,rax
 	call rpc_r
 	mov rdi,r9
@@ -17,7 +17,7 @@ fact:
 # 	∐ 0'
 agl_0_0:
 # 	» .. |~ 0' ⊢ 1'
-# 	$ 1' ⊢ ,0'
+# 	$ 1' ⊢ ,0' : ,@[0].(ℤn)◂(ℤ_u) ≃ ∐[{ } { } ]
 	mov r8,rdi
 	call rpc_r
 	mov rax,r9
@@ -26,7 +26,7 @@ agl_0_0:
 # 	∎ 0'
 # 	∐ 0'
 agl_0_1:
-# 	$ 2' ⊢ ,1',3'
+# 	$ 2' ⊢ ,1',3' : ,@[0].(ℤn)◂(ℤ_u) ≃ ∐[{ } { } ],@[0].(ℤn)◂(ℤ_u) ≃ ∐[{ } { } ]
 	mov r8,rsi
 	call rpc_r
 	mov rdi,r9
@@ -36,7 +36,7 @@ agl_0_1:
 	mov r8,rsi
 	call rmv_r
 # 	» .. |~ 1' ⊢ 2'
-# 	$ 3' ⊢ ,1',4'
+# 	$ 3' ⊢ ,1',4' : ,@[0].t52'(0) ≃ ∐[{ } { } ],@[0].t52'(0) ≃ ∐[{ } { } ]
 	mov r8,rdx
 	call rpc_r
 	mov rdi,r9
@@ -45,7 +45,7 @@ agl_0_1:
 	mov rcx,r9
 	mov r8,rdx
 	call rmv_r
-# 	$ 4' ⊢ ,3',5'
+# 	$ 4' ⊢ ,3',5' : ,@[0].t53'(0) ≃ ∐[{ } { } ],@[0].t53'(0) ≃ ∐[{ } { } ]
 	mov r8,rcx
 	call rpc_r
 	mov rdx,r9
@@ -55,7 +55,7 @@ agl_0_1:
 	mov r8,rcx
 	call rmv_r
 # 	» .. |~ 3' ⊢ 4'
-# 	$ 5' ⊢ ,3',6'
+# 	$ 5' ⊢ ,3',6' : ,@[0].t55'(0) ≃ ∐[{ } { } ],@[0].t55'(0) ≃ ∐[{ } { } ]
 	mov r8,r12
 	call rpc_r
 	mov rdx,r9
@@ -64,32 +64,22 @@ agl_0_1:
 	mov r13,r9
 	mov r8,r12
 	call rmv_r
-# 	» .. |~ 3' ⊢ 5'
-# 	$ 6' ⊢ ,3',7'
+# 	$ 6' ⊢ ,5',7' : ,@[0].t56'(0) ≃ ∐[{ } { } ],@[0].t56'(0) ≃ ∐[{ } { } ]
 	mov r8,r13
 	call rpc_r
-	mov rdx,r9
+	mov r12,r9
 	mov r8,r13
 	call rpc_r
 	mov r14,r9
 	mov r8,r13
 	call rmv_r
-# 	$ 7' ⊢ ,6',8'
-	mov r8,r14
-	call rpc_r
-	mov r13,r9
-	mov r8,r14
-	call rpc_r
-	mov r15,r9
-	mov r8,r14
-	call rmv_r
-# 	» .. |~ 6' ⊢ 7'
-# 	» .. |~ 8' ⊢ 6'
-# 	◂ 7',6' ⊢ 8'
-# 	◂ 5',{ 3' 8' } ⊢ 6'
-# 	◂ 4',6' ⊢ 3'
-# 	◂ 2',{ 1' 3' } ⊢ 4'
-# 	$ 4' ⊢ ,1'
+# 	» .. |~ 5' ⊢ 6'
+# 	» .. |~ 7' ⊢ 5'
+# 	◂ 6',5' ⊢ 7' : @[0].(ℤn)◂(ℤ_u) ≃ ∐[{ } { } ]
+# 	◂ 4',{ 3' 7' } ⊢ 5' : @[0].(ℤn)◂(ℤ_u) ≃ ∐[{ } { } ]
+# 	fact5' ⊢ 3' : @[0].(ℤn)◂(ℤ_u) ≃ ∐[{ } { } ]
+# 	◂ 2',{ 1' 3' } ⊢ 4' : @[0].(ℤn)◂(ℤ_u) ≃ ∐[{ } { } ]
+# 	$ 4' ⊢ ,1' : ,@[0].(ℤn)◂(t63'(0)) ≃ ∐[{ } { } ]
 	mov r8,rcx
 	call rpc_r
 	mov rdi,r9
@@ -100,27 +90,31 @@ f0:
 # 	|» 0'
 # 	∎ 0'
 rpc_r:
-  mov r10,r8
-  shr r10,1
+  rcr r8
   jnc rpc_r_ptr
 rpc_r_v:
+  rcl r8
   mov r9,r8
   jmp rpc_r_end
 rpc_r_ptr:
-    mov r9,[r8]
-    add r9,1
-    mov [r8],r9
+  rcl r8
+  mov r9,[r8]
+  add r9,1
+  mov [r8],r9
   shr r9,32
   push r8
+  jmp rpc_r_ptr_lp
+rpc_r_ptr_lp_seq:
+  rcl [r8]
 rpc_r_ptr_lp:
   add r8,1
   cmp r9,0
   pop r9
   je rpc_r_end
   sub r9,1
-  mov r10,[r8]
-  shr r10,1
-  jc rpc_r_ptr_lp
+  rcr [r8]
+  jc rpc_r_ptr_lp_seq
+  rcl [r8]
   push r8
   push r9
   mov r8,[r8]
@@ -133,14 +127,17 @@ rpc_r_inc_ptr:
   add r9,1
   mov [r8],r9
   shr r9,32
+  jmp rpc_r_inc_ptr_lp
+rpc_inc_ptr_lp_seq:
+  rcl [r8]
 rpc_r_inc_ptr_lp:
   add r8,1
   cmp r9,0
-  je rpc_r_inc_ptr_end
+  je rpc_r_end
   sub r9,1
-  mov r10,[r8]
-  shr r10,1
-  jc rpc_r_inc_ptr_lp
+  rcr [r8]
+  jc rpc_r_inc_ptr_lp_seq
+  rcl [r8]
   push r8
   push r9
   mov r8,[r8]
@@ -148,7 +145,5 @@ rpc_r_inc_ptr_lp:
   pop r9
   pop r8
   jmp rpc_r_inc_ptr_lp
-rpc_r_inc_ptr_end:
-  ret
 rpc_r_end:
   ret
