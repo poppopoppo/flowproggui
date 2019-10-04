305627ca41dfc518c75b8f1f693cded2fdd5625c
poppopoppo <g6.8133251p2@gmail.com>
2019-10-04 14:57:00 +0900

    emt_get_ptn inc_r

--- vm/ir0/adt.mdls.s
+++ vm/ir0/adt.mdls.s
@@ -11,7 +11,7 @@ main:
 	mov rdi,rax
 	clc
 	cmp rdi,0
-	call f3
+	call f0
 	mov rdi,rax
 	mov rsi,str_ret
 	call pnt
@@ -36,21 +36,33 @@ emt_etr_c_lb_0:
 ; emt_set_ptn {  }
 	pop rbx
 _f2:
-; 	$ {  } ⊢ ,_r26,_r27 : ,{ },{ }
-; emt_ptn_crt_ptn {  },_r26
+; 	$ {  } ⊢ ,_r33,_r34 : ,{ },{ }
+	pushf
+	popf
+; emt_ptn_crt_ptn {  },_r33
 ; emt_get_ptn {  }
+	push rbx
+	push r14
 	mov rdi,0
 	call mlc
-	mov r8,rax
+	mov rbx,rax
+	mov r8,rbx
+	pop r14
+	pop rbx
 	clc
 	and r12,~0b1
-; emt_ptn_crt_ptn {  },_r27
+; emt_ptn_crt_ptn {  },_r34
 ; emt_get_ptn {  }
+	push rbx
+	push r14
 	push r8
 	mov rdi,0
 	call mlc
-	mov r9,rax
+	mov rbx,rax
 	pop r8
+	mov r9,rbx
+	pop r14
+	pop rbx
 	clc
 	and r12,~0b10
 ; ; emt_dec_ptn {  }
@@ -58,66 +70,113 @@ _f2:
 	mov r11,0xff
 	mov r10,r11
 ; 	$ { 2' {  } } ⊢ ,v0 : ,{ r64 { } }
+	pushf
+	push r8
+	push r9
+	push r10
+	mov rsi,str_ret
+	mov rdi,r10
+	bt r12,2
+	call pnt
+	call pnt_str_ret
+	pop r10
+	pop r9
+	pop r8
+	push r8
+	push r9
+	push r10
+	mov rsi,str_ret
+	mov rdi,r8
+	bt r12,0
+	call pnt
+	call pnt_str_ret
+	pop r10
+	pop r9
+	pop r8
+	push r8
+	push r9
+	push r10
+	mov rsi,str_ret
+	mov rdi,r9
+	bt r12,1
+	call pnt
+	call pnt_str_ret
+	pop r10
+	pop r9
+	pop r8
+	popf
 ; emt_ptn_crt_ptn { 2' {  } },v0
 ; emt_get_ptn { 2' {  } }
+	push rbx
+	push r14
 	push r8
 	push r9
 	push r10
 	mov rdi,2
 	call mlc
-	mov r11,rax
+	mov rbx,rax
 	pop r10
 	pop r9
 	pop r8
-	push r11
 ; emt_get_ptn 2'
 	mov r11,r10
 	stc
 	mov rdx,r11
-	pop rdi
+	mov rdi,rbx
 	mov rsi,0
 	call exc
-	mov r11,rdi
-	push r11
 ; emt_get_ptn {  }
+	push rbx
+	push r14
 	push r8
 	push r9
 	push r10
 	mov rdi,0
 	call mlc
-	mov r11,rax
+	mov rbx,rax
 	pop r10
 	pop r9
 	pop r8
+	mov r11,rbx
+	pop r14
+	pop rbx
 	clc
 	mov rdx,r11
-	pop rdi
+	mov rdi,rbx
 	mov rsi,1
 	call exc
-	mov r11,rdi
+	mov r11,rbx
+	pop r14
+	pop rbx
 	clc
 	and r12,~0b1000
 ; ; emt_dec_ptn { 2' {  } }
 ; unboxed
 ; 	∎ 3'
-;clear  0'~_r26 1'~_r27
+	push rbx
+; emt_get_ptn 3'
+	push rdi
+	mov rdi,r11
+	call inc_r
+	mov r11,rdi
+	pop rdi
+	mov rbx,r11
+	clc
+	push rbx
+;clear  0'~_r33 1'~_r34
 	bt r12,0
-	jc clear_lb_8
+	jc clear_lb_20
 	push r9
 	mov rdi,r8
 	call dec_r
 	pop r9
-clear_lb_8:
+clear_lb_20:
 	bt r12,1
-	jc clear_lb_9
+	jc clear_lb_21
 	mov rdi,r9
 	call dec_r
-clear_lb_9:
-	push rbx
-; emt_get_ptn 3'
-	mov rbx,r11
-	clc
-	mov rax,rbx
+clear_lb_21:
+	pop rax
 	pop rbx
 	ret
 f3:
@@ -140,13 +199,19 @@ emt_etr_c_lb_1:
 	pop rbx
 _f3:
 ; ⟦⟧
-; emt_ptn_crt_ptn {  },_r29
+; emt_ptn_crt_ptn {  },_r39
 ; emt_get_ptn {  }
+	push rbx
+	push r14
 	mov rdi,0
 	call mlc
-	mov r8,rax
+	mov rbx,rax
+	mov r8,rbx
+	pop r14
+	pop rbx
 	clc
 	and r12,~0b1
+; ; emt_dec_ptn {  }
 	push r8
 	mov rdi,2
 	call mlc
@@ -161,41 +226,63 @@ _f3:
 	clc
 	call exc
 	mov QWORD r8,rdi
-; 	$ 0' ⊢ ,l0 : ,@[0].(⟦⟧)◂(t24'(0)) ≃ ∐[{ } { t24'(0) @[0] } ]
+; 	$ 0' ⊢ ,l0 : ,@[0].(⟦⟧)◂(t31'(0)) ≃ ∐[{ } { t31'(0) @[0] } ]
+	pushf
+	push r8
+	mov rsi,str_ret
+	mov rdi,r8
+	bt r12,0
+	call pnt
+	call pnt_str_ret
+	pop r8
+	popf
 ; emt_ptn_crt_ptn 0',l0
 ; emt_set_ptn 1'
 ; unknown
 	bt r12,0
-	jc lb_4
+	jc lb_14
 	push rdi
 	mov rdi,r8
 	call inc_r
-	pop rdi
 	mov r9,rdi
+	pop rdi
 	and r12,~0b10
-	jmp lb_5
-lb_4:
+	jmp lb_15
+lb_14:
 	mov r9,r8
 	or r12,0b10
-lb_5:
+lb_15:
 ; ; emt_dec_ptn 0'
 ; unknown
 	bt r12,0
-	jc lb_6
+	jc lb_16
 	push r8
 	push r9
 	mov rdi,r8
 	call dec_r
 	pop r9
 	pop r8
-lb_6:
+lb_16:
 ; 	∎ 1'
-;clear 
 	push rbx
 ; emt_get_ptn 1'
+	jc lb_17
+; unboxed
+	push rdi
+	mov rdi,r9
+	call inc_r
+	mov r9,rdi
+	pop rdi
+	clc
+	jmp lb_18
+lb_17:
+; boxed
 	mov rbx,r9
-	bt r12,1
-	mov rax,rbx
+	stc
+lb_18:
+	push rbx
+;clear 
+	pop rax
 	pop rbx
 	ret
 f0:
@@ -217,50 +304,446 @@ emt_etr_c_lb_2:
 ; emt_set_ptn {  }
 	pop rbx
 _f0:
-; 	$ {  } ⊢ ,_r21 : ,{ }
-; emt_ptn_crt_ptn {  },_r21
+; 	$ {  } ⊢ ,_r22,_r23 : ,{ },{ }
+	pushf
+	popf
+; emt_ptn_crt_ptn {  },_r22
 ; emt_get_ptn {  }
+	push rbx
+	push r14
 	mov rdi,0
 	call mlc
-	mov r8,rax
+	mov rbx,rax
+	mov r8,rbx
+	pop r14
+	pop rbx
 	clc
 	and r12,~0b1
-; ; emt_dec_ptn {  }
-; 	$ { {  } } ⊢ ,l0 : ,{ { } }
-; emt_ptn_crt_ptn { {  } },l0
-; emt_get_ptn { {  } }
-	push r8
-	mov rdi,1
-	call mlc
-	mov r9,rax
-	pop r8
-	push r9
+; emt_ptn_crt_ptn {  },_r23
 ; emt_get_ptn {  }
+	push rbx
+	push r14
 	push r8
 	mov rdi,0
 	call mlc
-	mov r9,rax
+	mov rbx,rax
 	pop r8
-	clc
-	mov rdx,r9
-	pop rdi
-	mov rsi,0
-	call exc
-	mov r9,rdi
+	mov r9,rbx
+	pop r14
+	pop rbx
 	clc
 	and r12,~0b10
-; ; emt_dec_ptn { {  } }
-; 	∎ 1'
-;clear  0'~_r21
+; ; emt_dec_ptn {  }
+; 	» 0xrff |~ 2' : r64
+	mov r11,0xff
+	mov r10,r11
+; ⟦⟧
+; emt_ptn_crt_ptn {  },_r36
+; emt_get_ptn {  }
+	push rbx
+	push r14
+	push r8
+	push r9
+	push r10
+	mov rdi,0
+	call mlc
+	mov rbx,rax
+	pop r10
+	pop r9
+	pop r8
+	mov r11,rbx
+	pop r14
+	pop rbx
+	clc
+	and r12,~0b1000
+; ; emt_dec_ptn {  }
+	push r8
+	push r9
+	push r10
+	push r11
+	mov rdi,2
+	call mlc
+	btr r12,3
+	mov rdi,rax
+	mov rsi,0
+	mov rdx,0
+	stc
+	call exc
+	mov rsi,1
+	pop rdx
+	clc
+	call exc
+	mov QWORD r11,rdi
+	pop r10
+	pop r9
+	pop r8
+; ⟦
+	pushf
+	push r8
+	push r9
+	push r10
+	push r11
+	mov rsi,str_ret
+	mov rdi,r10
+	bt r12,2
+	call pnt
+	call pnt_str_ret
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	push r8
+	push r9
+	push r10
+	push r11
+	mov rsi,str_ret
+	mov rdi,r11
+	bt r12,3
+	call pnt
+	call pnt_str_ret
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	push r8
+	push r9
+	push r10
+	push r11
+	mov rsi,str_ret
+	mov rdi,r8
 	bt r12,0
-	jc clear_lb_3
+	call pnt
+	call pnt_str_ret
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	push r8
+	push r9
+	push r10
+	push r11
+	mov rsi,str_ret
+	mov rdi,r9
+	bt r12,1
+	call pnt
+	call pnt_str_ret
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	popf
+; emt_ptn_crt_ptn { 2' 3' },_r38
+; emt_get_ptn { 2' 3' }
+	push rbx
+	push r14
+	push r8
+	push r9
+	push r10
+	push r11
+	mov rdi,2
+	call mlc
+	mov rbx,rax
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+; emt_get_ptn 2'
+	mov rcx,r10
+	stc
+	mov rdx,rcx
+	mov rdi,rbx
+	mov rsi,0
+	call exc
+; emt_get_ptn 3'
+	jc lb_3
+; unboxed
+	push rdi
+	mov rdi,r11
+	call inc_r
+	mov r11,rdi
+	pop rdi
+	clc
+	jmp lb_4
+lb_3:
+; boxed
+	mov rcx,r11
+	stc
+lb_4:
+	mov rdx,rcx
+	mov rdi,rbx
+	mov rsi,1
+	call exc
+	mov rcx,rbx
+	pop r14
+	pop rbx
+	clc
+	and r12,~0b10000
+; ; emt_dec_ptn { 2' 3' }
+; unknown
+	bt r12,3
+	jc lb_5
+	push r8
+	push r9
+	push r10
+	push r11
+	push rcx
+	mov rdi,r11
+	call dec_r
+	pop rcx
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+lb_5:
+; unboxed
+	push r8
+	push r9
+	push rcx
+	mov rdi,2
+	call mlc
+	mov rdi,rax
+	pushf
+ push rax
+    push rdi
+    push rsi
+    push rdx
+    push r8
+    push r9
+    push r10
+    push r11
+	mov rsi,str_ret
+	mov rdi,rdi
+
+	clc
+	call pnt
+	call pnt_str_ret
+ pop r11
+  pop r10
+  pop r9
+  pop r8
+  pop rdx
+  pop rsi
+  pop rdi
+  pop rax
+	popf
+	mov rsi,0
+	mov rdx,1
+	stc
+	call exc
+	mov rsi,1
+	call dbg
+	pushf
+ push rax
+    push rdi
+    push rsi
+    push rdx
+    push r8
+    push r9
+    push r10
+    push r11
+	mov rsi,str_ret
+	mov rdi,rdi
+	clc
+	call pnt
+	call pnt_str_ret
+ pop r11
+  pop r10
+  pop r9
+  pop r8
+  pop rdx
+  pop rsi
+  pop rdi
+  pop rax
+	popf
+	pushf
+ push rax
+    push rdi
+    push rsi
+    push rdx
+    push r8
+    push r9
+    push r10
+    push r11
+	mov rsi,str_ret
+	mov rdi,rdi
+	stc
+	call pnt
+	call pnt_str_ret
+ pop r11
+  pop r10
+  pop r9
+  pop r8
+  pop rdx
+  pop rsi
+  pop rdi
+  pop rax
+	popf
+	pop rdx
+	pushf
+ push rax
+    push rdi
+    push rsi
+    push rdx
+    push r8
+    push r9
+    push r10
+    push r11
+	mov rsi,str_ret
+	mov rdi,rdx
+	clc
+	call pnt
+	call pnt_str_ret
+ pop r11
+  pop r10
+  pop r9
+  pop r8
+  pop rdx
+  pop rsi
+  pop rdi
+  pop rax
+	popf
+	bt r12,4
+	call exc
+	mov QWORD r10,rdi
+	btr r12,2
+	call dbg
+	pushf
+ push rax
+    push rdi
+    push rsi
+    push rdx
+    push r8
+    push r9
+    push r10
+    push r11
+	mov rsi,str_ret
+	mov rdi,rdi
+
+	clc
+	call pnt
+	call pnt_str_ret
+ pop r11
+  pop r10
+  pop r9
+  pop r8
+  pop rdx
+  pop rsi
+  pop rdi
+  pop rax
+	popf
+	pushf
+	push r8
+	push r9
+	push r10
+	mov rsi,str_ret
+	mov rdi,r10
+	bt r12,2
+	call pnt
+	call pnt_str_ret
+	pop r10
+	pop r9
+	pop r8
+	popf
+	pop r9
+	pop r8
+; 	$ 2' ⊢ ,l0 : ,@[0].(⟦⟧)◂(r64) ≃ ∐[{ } { r64 @[0] } ]
+	pushf
+	push r8
+	push r9
+	push r10
+	mov rsi,str_ret
+	mov rdi,r10
+	bt r12,2
+	call pnt
+	call pnt_str_ret
+	pop r10
+	pop r9
+	pop r8
+	push r8
+	push r9
+	push r10
+	mov rsi,str_ret
+	mov rdi,r8
+	bt r12,0
+	call pnt
+	call pnt_str_ret
+	pop r10
+	pop r9
+	pop r8
+	push r8
+	push r9
+	push r10
+	mov rsi,str_ret
+	mov rdi,r9
+	bt r12,1
+	call pnt
+	call pnt_str_ret
+	pop r10
+	pop r9
+	pop r8
+	popf
+; emt_ptn_crt_ptn 2',l0
+; emt_set_ptn 3'
+; unknown
+	bt r12,2
+	jc lb_7
+	push rdi
+	mov rdi,r10
+	call inc_r
+	mov r11,rdi
+	pop rdi
+	and r12,~0b1000
+	jmp lb_8
+lb_7:
+	mov r11,r10
+	or r12,0b1000
+lb_8:
+; ; emt_dec_ptn 2'
+; unknown
+	bt r12,2
+	jc lb_9
+	push r8
+	push r9
+	push r10
+	push r11
+	mov rdi,r10
+	call dec_r
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+lb_9:
+; 	∎ 3'
+	push rbx
+; emt_get_ptn 3'
+	jc lb_12
+; unboxed
+	push rdi
+	mov rdi,r11
+	call inc_r
+	mov r11,rdi
+	pop rdi
+	clc
+	jmp lb_13
+lb_12:
+; boxed
+	mov rbx,r11
+	stc
+lb_13:
+	push rbx
+;clear  0'~_r22 1'~_r23
+	bt r12,0
+	jc clear_lb_10
+	push r9
 	mov rdi,r8
 	call dec_r
-clear_lb_3:
-	push rbx
-; emt_get_ptn 1'
-	mov rbx,r9
-	clc
-	mov rax,rbx
+	pop r9
+clear_lb_10:
+	bt r12,1
+	jc clear_lb_11
+	mov rdi,r9
+	call dec_r
+clear_lb_11:
+	pop rax
 	pop rbx
 	ret
