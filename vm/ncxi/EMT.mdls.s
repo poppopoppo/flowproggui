54b43f9050042ba8eb8c3d49b05e7907331b367e
poppopoppo <g6.8133251p2@gmail.com>
2020-01-23 00:58:48 +0900

    lc_coce stt call r64

--- vm/ncxi/EMT.mdls.s
+++ vm/ncxi/EMT.mdls.s
@@ -7,20 +7,26 @@ global _start
 _start:
 	jmp RTM_0
 ETR_24: ; f0 0t' ⊢ 0t' : (_r64→_r64)
+;; %0~0t' 
 ; _inc 0t' ⊢ 0t'
 	add r13,1
+;; %1~0t' 
 ; ∎ 0t'
 ; .mov_ptn 0t' ⊢ 0t'
 	mov r14,r13
 	mov r13,r14
 	ret
 ETR_25: ; f1 { 0t' 1t' } ⊢ { 0t' 1t' } : ({ _r64 _r64 }→{ _r64 _r64 })
+;; %3~1t' %2~0t' 
 ; _inc 0t' ⊢ 0t'
 	add r13,1
+;; %4~0t' %3~1t' 
 ; _inc 1t' ⊢ 1t'
 	add r14,1
+;; %5~1t' %4~0t' 
 ; _inc 1t' ⊢ 1t'
 	add r14,1
+;; %6~1t' %4~0t' 
 ; ∎ { 0t' 1t' }
 ; .mov_ptn { 0t' 1t' } ⊢ { 0t' 1t' }
 	mov r8,r13
@@ -30,9 +36,12 @@ ETR_25: ; f1 { 0t' 1t' } ⊢ { 0t' 1t' } : ({ _r64 _r64 }→{ _r64 _r64 })
 	ret
 RTM_0:
 	push RTM_1
+;; 
 ; $ %[ 0r ] ⊢ %[ 0r ]
+;; %7~%[ 0r ] 
 ; _inc %[ 0r ] ⊢ 0t'
 	mov r13,1
+;; %8~0t' 
 ; #24 0t' ⊢ 0t'
 	lea rsp,[rsp-8*0]
 	lea rsp,[rsp-8*0]
@@ -40,14 +49,33 @@ RTM_0:
 	mov r14,r13
 	mov r13,r14
 	call ETR_24
+;; %9~0t' 
 ; $ %[ 3r ] ⊢ %[ 3r ]
-; #25 { 0t' %[ 3r ] } ⊢ { 0t' 1t' }
+;; %10~%[ 3r ] %9~0t' 
+; $ %[ 4r ] ⊢ %[ 4r ]
+;; %11~%[ 4r ] %10~%[ 3r ] %9~0t' 
+; #24 %[ 4r ] ⊢ 1t'
 	lea rsp,[rsp-8*0]
+	lea rsp,[rsp-8*1]
+	mov QWORD [rsp+8*0],r13
+; .mov_ptn %[ 4r ] ⊢ 0t'
+	mov r13,4
+	call ETR_24
+	mov r14,r13
+	mov r13,[rsp+8*0]
+;; %12~1t' %10~%[ 3r ] %9~0t' 
+; #25 { 0t' %[ 3r ] } ⊢ { 0t' 2t' }
 	lea rsp,[rsp-8*0]
+	lea rsp,[rsp-8*1]
+	mov QWORD [rsp+8*0],r14
 ; .mov_ptn { 0t' %[ 3r ] } ⊢ { 0t' 1t' }
 	mov r8,r13
 	mov r13,r8
+	mov r14,3
 	call ETR_25
+	mov r8,r14
+	mov r14,[rsp+8*0]
+;; %14~2t' %13~0t' %12~1t' 
 ; ∎ { }
 ; .mov_ptn { } ⊢ { }
 	ret
