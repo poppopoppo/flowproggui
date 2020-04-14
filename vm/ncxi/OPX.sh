460c642e3bc05da49e30ef31cb4f37cbef4a7c07
poppopoppo <g6.8133251p2@gmail.com>
2020-04-15 00:08:21 +0900

    self host loop !! SHX

--- vm/ncxi/OPX.sh
+++ vm/ncxi/OPX.sh
@@ -6,3 +6,6 @@ chmod +rw EMT.mdls.s
 chmod +rw SGN.txt
 ./opx main_sh.mdls syntax.mdls start_sh.mdls
 
+yasm -g dwarf2 -f elf64 EMT.mdls.s -o out.o 
+clang -Wall stub.c out.o -nostartfiles -no-pie -pg -g -O0 -o EMT.mdls.exe 
+ln -f EMT.mdls.exe EMT 
