4097b9e93e5e4e814bc2a7b5002ebf671c61354c
poppopoppo <g6.8133251p2@gmail.com>
2020-04-17 23:20:39 +0900

    slv.cst

--- vm/ncxi/SHL.sh
+++ vm/ncxi/SHL.sh
@@ -1,5 +1,2 @@
 cp SH.mdls.exe SHX0 
 ./SHX main_sh.mdls syntax.mdls start_sh.mdls 
-yasm -g dwarf2 -f elf64 SH.mdls.s -o out.o
-clang  -Wall stub.c out.o -nostartfiles -no-pie -pg -g -O0 -o SH.mdls.exe
-ln -f SH.mdls.exe SHX
--- vm/ncxi/SHX.sh
+++ vm/ncxi/SHX.sh
@@ -1,4 +1,3 @@
-./SHX main_sh.mdls syntax.mdls start_sh.mdls 
+./opxcc main_sh.mdls syntax.mdls start_sh.mdls 
 yasm -g dwarf2 -f elf64 SH.mdls.s -o out.o
-clang  -Wall stub.c out.o -nostartfiles -no-pie -pg -g -O0 -o SH0.mdls.exe
-ln -f SH0.mdls.exe SH0
+clang  -Wall stub.c out.o -nostartfiles -no-pie -pg -g -O2 -o out.exe
