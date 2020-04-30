c09b62f7be9f5154a54623c5e038d02443e63cf8
poppopoppo <g6.8133251p2@gmail.com>
2020-05-01 04:36:06 +0900

    sig segv handled !!

--- vm/ncxi/opxc-1.6/stub.c
+++ vm/ncxi/opxc-1.6/stub.c
@@ -4,7 +4,29 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <sys/mman.h>
 
+void* alc_gd_buf(int* ps){
+	int pagesize = sysconf(_SC_PAGE_SIZE);
+	if (pagesize == -1){ return NULL; }
+
+	char* buffer = memalign(pagesize, 2 * pagesize);
+	if (buffer == NULL){
+		return NULL; }
+
+	if (mprotect(buffer + pagesize, pagesize,
+							 PROT_NONE) == -1){ 
+		return NULL;
+							 }
+		*ps = pagesize; 
+		return buffer;
+}
 int set_handler(int n,void* f){
 	struct sigaction a;
 	a.sa_flags = SA_SIGINFO;
