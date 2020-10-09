61ca866d96be4fc11f18390bf281d36de157578f
poppopoppo <g6.8133251p2@gmail.com>
2020-10-09 16:53:25 +0900

    -e option

---
+++ vm/ncxi/opxc-2.2/START.s
@@ -0,0 +1,56 @@
+; ini_prc 
+	C_CALL ini_prc 
+; set signal handler 
+	mov rdi,SIG_SEGV 
+	mov rsi,sig_hdl 
+	C_CALL set_handler 
+	mov QWORD [SIG_ETR],sig_dft 
+	mov QWORD [SIG_FLG],0 
+	mov rdi,0 
+ini_ss_rcd:
+ mov QWORD [SS_RCD_N+8*rdi],0 
+	mov QWORD [SS_RCD_C+8*rdi],0
+	mov QWORD [SS_RCD_TOP+8*rdi],~0
+	add rdi,1 
+	cmp rdi,RCD_N
+	jnz ini_ss_rcd  
+	
+	 
+
+; init emt buffer 
+	mov rdi,PAGE_SIZE
+	C_CALL alc_gd_buf
+	mov QWORD [GD_BUF_PT],rax
+	mov QWORD [GD_BUF_IT],rax
+	mov rax,0 
+	mov rdi,fmt_d 
+	mov rsi,QWORD [PAGE_SIZE]
+	C_CALL printf
+
+	mov rdi,0
+	mov QWORD [EMT_BUF],0
+	mov QWORD [EMT_BUF_OFS],0
+
+; _args 
+	mov rdi,QWORD [rsp]
+ lea rdi,[rdi+1]
+	mov rsi,8 
+	xor rax,rax 
+	C_CALL calloc
+	pop r13
+	mov QWORD [rax],r13
+	xor r14,r14
+	mov rbx,rax
+args_lp:
+	cmp r14,r13
+	jz args_lp_end
+	pop rdi
+	call s8_of_c_stg 
+	mov QWORD [rbx+8+8*r14],rax
+	add r14,1 
+	jmp args_lp 
+args_lp_end:
+	mov GLX(127),rbx
+ ; exn_root 
+	push exn_dft 
+	push exn_dft
